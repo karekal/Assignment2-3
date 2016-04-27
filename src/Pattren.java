9e00beb801554fb68bfbf92ce7affaf8f28b5d0f
srima karekal <srima589.karekal@gmail.com>
2016-04-27 09:25:32 +0530

    commit

---
+++ src/Pattren.java
@@ -0,0 +1,21 @@
+
+public class Pattren {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i,j;
+for(i=1;i<=5;i++){
+	for(j=1;j<=i;j++){
+		System.out.print(j);
+	}
+System.out.println(" ");
+}
+for(i=5;i>=1;i--){
+	for(j=1;j<i;j++){
+		System.out.print(j);
+	}
+System.out.println(" ");
+}
+	}
+
+}
